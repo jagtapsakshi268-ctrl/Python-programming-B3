f920630c172d9962140c347bc8e1d765f39dd43c
jagtapsakshi268-ctrl <jagtapsakshi268@gmail.com>
2026-02-05 11:22:15 +0530

    Python Programming Exp1

---
+++ exp1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 10:54:47 2026
+
+@author: User
+"""
+
+name = input("Enter your name:")
+age =input  ("Enter your age:")
+roll_no =input  ("Enter your roll no:")
+division = input("Enter your division:")
+blood_group = input("Enter your blood group:")
+college = input("Enter your college:")
+print("\n----student information----")
+
+print("Name:",name)
+print("Age:",age)
+print("Roll number:",roll_no)
+print("Division:",division)
+print("Blood group:",blood_group)
+print("College:",college)
+ 
