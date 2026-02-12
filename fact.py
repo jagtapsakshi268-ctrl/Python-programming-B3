b89318943bff51d6cadd0311aa1ffa564679c9f7
jagtapsakshi268-ctrl <jagtapsakshi268@gmail.com>
2026-02-12 11:15:46 +0530

    fact

---
+++ fact.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 12 10:32:42 2026
+
+@author: User
+"""
+
+n = int(input("Enter number: "))
+fact = 1
+for i in range(1, n + 1):
+    fact = fact * i
+print("Factorial:", fact)
